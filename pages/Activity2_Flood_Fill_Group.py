87c15e4c7867f285bdd980bc424d89caf7c88c19
hydraadra112 <126627743+hydraadra112@users.noreply.github.com>
2023-03-10 10:21:40 +0800

    Update Activity2_Flood_Fill_Group.py

--- pages/Activity2_Flood_Fill_Group.py
+++ pages/Activity2_Flood_Fill_Group.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import numpy as np
+import random as random
 import matplotlib.pyplot as plt
 
 st.title("Activity2\nGroup6\nFloodFill")
@@ -10,22 +11,29 @@ two_d_arr = np.array([[0, 0, 0, 0],
                     [0, 1, 1, 0],
                     [0, 0, 0, 0]])
 
+random.shuffle(two_d_arr)
+
 st.write('First Plot')
 plt.imshow(two_d_arr, interpolation='none', cmap='inferno')
 plt.show()
 st.pyplot(fig)
 
 color = st.slider('Enter Color Number to Fill (0-16):', 0, 16, 1)
+st.write('Changing Color Process')
 
 for row in range(len(two_d_arr)):
     for col in range(len(two_d_arr)):
       
         if two_d_arr[row][col] == 1:
           continue
-          
+        
         two_d_arr[row][col] = color
+        
+        plt.imshow(two_d_arr, interpolation='none', cmap='inferno')
+        plt.show()
+        st.pyplot(fig)
 
-st.write('Second Plot with Color Changed')
+st.write('Finished Product')
 plt.imshow(two_d_arr, interpolation='none', cmap='inferno')
 plt.show()
 st.pyplot(fig)
