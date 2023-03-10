06bce936922f4fdf1f96d8fe49064daf0ddc7114
hydraadra112 <126627743+hydraadra112@users.noreply.github.com>
2023-03-10 22:03:54 +0800

    Update Activity1_Bresenhams_Line.py

--- pages/Activity1_Bresenhams_Line.py
+++ pages/Activity1_Bresenhams_Line.py
@@ -5,48 +5,41 @@ st.title("Activity1\nGroup6\nBresenhamsLine")
 
 def BresenhamLine(x1, y1, x2, y2, color):
     
-    x, y = x1, y1
-    dx = abs(x2 - x1)
-    dy = abs(y2 - y1)
-    
-    grad = dy/float(dx)
-    
-    x3 = float((x2 + x1) / 2)
-    y3 = ((y2 + y1) / 2)
-   
+  fig = plt.figure()
 
-    if grad > 1:
+    x,y = x1,y1
+    dx = abs(x2 - x1)
+    dy = abs(y2 -y1)
+    gradient = dy/float(dx)
+
+    if gradient > 1:
         dx, dy = dy, dx
         x, y = y, x
         x1, y1 = y1, x1
         x2, y2 = y2, x2
 
-    p = 2 * dy - dx
+    p = 2*dy - dx
+    # print(f"x = {x}, y = {y}")
+    # Initialize the plotting points
     xcoordinates = [x]
     ycoordinates = [y]
 
-    
-    steps = abs(dx) if abs(dx) > abs(dy) else abs(dy)
-
-    fig = plt.figure()
-    
-    for i in range(0, int(steps + 1)):
+    for k in range(2, dx + 2):
         if p > 0:
             y = y + 1 if y < y2 else y - 1
             p = p + 2 * (dy - dx)
-            
         else:
-            p = p + 2  * dy
+            p = p + 2 * dy
 
         x = x + 1 if x < x2 else x - 1
 
-        print('x = %s, y = %s' % (x, y))
+        # print(f"x = {x}, y = {y}")
         xcoordinates.append(x)
         ycoordinates.append(y)
-        
-    plt.plot(xcoordinates, ycoordinates)
-    plt.plot(x3, y3, marker = "o", markersize = 5, markerfacecolor = "red")
-    
+
+    plt.plot(xcoordinates, ycoordinates, color, marker='s', markersize=5)
+    plt.plot(round(dx/2 + x1), round(dy/2 + y1), 'bo', markersize=5)
+
     plt.show()
     st.pyplot(fig)
     
