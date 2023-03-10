5823201e80b6e644acfaf96acf614586d4d59c69
hydraadra112 <126627743+hydraadra112@users.noreply.github.com>
2023-03-10 21:50:48 +0800

    di mo sure

--- pages/Activity1_Bresenhams_Line.py
+++ pages/Activity1_Bresenhams_Line.py
@@ -1,43 +1,69 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 st.title("Activity1\nGroup6\nBresenhamsLine")
-def DDALine (x1, y1, x2, y2, color):
-    dx = abs(x1 - x2)
-    dy = abs(y1 - y2)
+
+
+def BresenhamLine(x1, y1, x2, y2, color):
+    
+    x, y = x1, y1
+    dx = abs(x2 - x1)
+    dy = abs(y2 - y1)
+    
+    grad = dy/float(dx)
+    
+    x3 = (x2 + x1) / 2
+    y3 = (y2 + y1) / 2
+   
+
+    if grad > 1:
+        dx, dy = dy, dx
+        x, y = y, x
+        x1, y1 = y1, x1
+        x2, y2 = y2, x2
+
+    p = 2 * dy - dx
+    xcoordinates = [x]
+    ycoordinates = [y]
+
     
     steps = abs(dx) if abs(dx) > abs(dy) else abs(dy)
+
+    fig = plt.figure()
     
-    Xinc = float(dx / steps)
-    Yinc = float(dy / steps)
-    
-    for i in range (0, int(steps+1)):
-        st.line_chart(x1, y1)
-        x1 += Xinc
-        y1 += Yinc
+    for i in range(0, int(steps + 1)):
+        if p > 0:
+            y = y + 1 if y < y2 else y - 1
+            p = p + 2 * (dy - dx)
+            
+        else:
+            p = p + 2  * dy
+
+        x = x + 1 if x < x2 else x - 1
+
+        print('x = %s, y = %s' % (x, y))
+        xcoordinates.append(x)
+        ycoordinates.append(y)
         
+    plt.plot(xcoordinates, ycoordinates)
+    plt.plot(x3, y3, marker = "o", markersize = 5, markerfacecolor = "red")
+    
     plt.show()
+    st.pyplot(fig)
     
-def main () :
+    st.write("Midpoint: ", int(x3), "&", int(y3))
+
+def main():
     
-    x1 = st.number_input('X1: ')
-    y1 = st.number_input('Y1: ')
-    x2 = st.number_input('X2: ')
-    y2 = st.number_input('Y2: ')
+    x = st.slider('X1: ', 0, 100, 1)
+    y = st.slider('Y1: ', 0, 100, 1)
+
+    x2 = st.slider('X2: ', 0, 100, 1)
+    y2 = st.slider('Y2: ', 0, 100, 1)
     
-    color = "b."
-    
-    x1 = 10
-    y1 = 15
-    x2 = 45
-    y2 = 67
-    
-    DDALine (x1, y1, x2, y2, color)
-    
-    #Enter X1: 10
-    #Enter Y1: 15
-    #Enter X2: 45
-    #Enter Y2: 67 
-    
+    color = "g." 
+    BresenhamLine(x, y, x2, y2, color)
+
+
 if __name__ == '__main__':
     main()
             
