64ce126356c9ca270119976c8706d580937de702
hydraadra112 <126627743+hydraadra112@users.noreply.github.com>
2023-03-10 10:10:37 +0800

    Update Activity2_Flood_Fill_Group.py

--- pages/Activity2_Flood_Fill_Group.py
+++ pages/Activity2_Flood_Fill_Group.py
@@ -5,15 +5,20 @@ import matplotlib.pyplot as plt
 st.title("Activity2\nGroup6\nFloodFill")
 fig = plt.figure()
 
-two_d_arr = np.array([[0, 1, 0], 
-                    [1, 1, 1], 
-                    [0, 1, 0]])
+two_d_arr = np.array([[0, 0, 0, 0],
+                    [0, 1, 1, 0], 
+                    [0, 1, 1, 0],
+                    [0, 0, 0, 0]])
 
 
 color = st.slider('Enter Color Number to Fill (0-16):', 0, 16, 1)
 
 for row in range(len(two_d_arr)):
     for col in range(len(two_d_arr)):
+      
+        if two_d_arr[row][col] == 1:
+          continue
+          
         two_d_arr[row][col] = color
         plt.imshow(two_d_arr, interpolation='none', cmap='gray_r')
         plt.show()
