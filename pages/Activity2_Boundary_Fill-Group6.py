cf41b98ef7b25f6b0f6364ce2f78773d99746453
hydraadra112 <126627743+hydraadra112@users.noreply.github.com>
2023-03-10 19:08:02 +0800

    Update Activity2_Boundary_Fill-Group6.py

--- pages/Activity2_Boundary_Fill-Group6.py
+++ pages/Activity2_Boundary_Fill-Group6.py
@@ -18,8 +18,6 @@ st.pyplot(fig)
 
 st.write(two_d_arr)
 
-row1 = int(st.number_input("Enter Row (0-2): "))
-col1 = int(st.number_input("Enter Index to Replace Color (0-2): "))
 color = int(st.number_input("Enter Color Number to Replace (0-16): "))
 
 row = 0
@@ -28,11 +26,25 @@ col = 0
 for row in range(len(two_d_arr[row])):
     for col in range(len(two_d_arr[col])):
         
-        two_d_arr[row1][col1] = color
-        
-        plt.imshow(two_d_arr, interpolation='none', cmap='Set3')
-        plt.show()
-        st.pyplot(fig)
+        if two_d_arr[row][col] == 1:
+            two_d_arr[row-1][col] = color # above to be changed
+            two_d_arr[row][col-1] = color # left to be changed
+            
+            for row1 in range(1, 5):
+                two_d_arr[row1][5] = color
+                
+            for col1 in range(1, 5):
+                two_d_arr[5][col1] = color
+                
+    plt.imshow(two_d_arr, interpolation='none', cmap='Set3')
+    plt.show()
+    st.pyplot(fig)
+                
+    
+st.write(two_d_arr)
+plt.imshow(two_d_arr, interpolation='none', cmap='Set3')
+plt.show()
+st.pyplot(fig)
         
         
         
