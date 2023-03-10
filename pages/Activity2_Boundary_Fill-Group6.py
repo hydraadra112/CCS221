bf1edd77106aef0e439fb31e605ba66d76be7ce8
hydraadra112 <126627743+hydraadra112@users.noreply.github.com>
2023-03-10 18:33:39 +0800

    di mo sure

--- pages/Activity2_Boundary_Fill-Group6.py
+++ pages/Activity2_Boundary_Fill-Group6.py
@@ -4,14 +4,23 @@ import matplotlib.pyplot as plt
 st.title("Activity2\nGroup6\nBoundaryFill")
 two_d_arr = np.array([[0, 0, 0, 0, 0, 0], 
                       [0, 1, 1, 1, 1, 0], 
-                      [0, 1, 0, 0, 1, 0],
-                      [0, 1, 0, 0, 1, 0],
+                      [0, 1, 1, 1, 1, 0],
+                      [0, 1, 1, 1, 1, 0],
                       [0, 1, 1, 1, 1, 0], 
                       [0, 0, 0, 0, 0, 0]])
 
 fig = plt.figure()
 
+st.write('Before Boundary Fill')
+plt.imshow(two_d_arr, interpolation='none', cmap='Set3')
+plt.show()
+st.pyplot(fig)
+
 st.write(two_d_arr)
+
+row1 = int(st.number_input("Enter Row (0-2): "))
+col1 = int(st.number_input("Enter Index to Replace Color (0-2): "))
+color = int(st.number_input("Enter Color Number to Replace (0-16): "))
 
 row = 0
 col = 0
@@ -19,26 +28,14 @@ col = 0
 for row in range(len(two_d_arr[row])):
     for col in range(len(two_d_arr[col])):
         
-        row1 = int(st.number_input("Enter Row (0-2): "))
-        col1 = int(st.number_input("Enter Index to Replace Color (0-2): "))
-        color = int(st.number_input("Enter Color Number to Replace (0-16): "))
-        
         two_d_arr[row1][col1] = color
         
         plt.imshow(two_d_arr, interpolation='none', cmap='Set3')
         plt.show()
         st.pyplot(fig)
         
-        again = int(st.number_input("\n1 to Change, 2 to Finish: "))
         
-        if again == 2:
-            plt.imshow(two_d_arr, interpolation='none', cmap='Set3')
-            plt.show()
-            
-            break
         
-    if again == 2:
-            break
 
 
 
