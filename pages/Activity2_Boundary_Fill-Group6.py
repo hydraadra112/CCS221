fbfed354bcf493610aa4b73f7a194f122c73cab2
hydraadra112 <126627743+hydraadra112@users.noreply.github.com>
2023-03-10 09:47:54 +0800

    Update Activity2_Boundary_Fill-Group6.py

--- pages/Activity2_Boundary_Fill-Group6.py
+++ pages/Activity2_Boundary_Fill-Group6.py
@@ -9,7 +9,9 @@ two_d_arr = np.array([[0, 0, 1, 1, 0, 0],
                       [0, 1, 0, 0, 1, 0], 
                       [0, 0, 1, 1, 0, 0]])
 
-print(two_d_arr)
+fig = plt.figure()
+
+st.write(two_d_arr)
 
 row = 0
 col = 0
@@ -17,17 +19,18 @@ col = 0
 for row in range(len(two_d_arr[row])):
     for col in range(len(two_d_arr[col])):
         
-        row1 = int(input("Enter Row (0-2): "))
-        col1 = int(input("Enter Index to Replace Color (0-2): "))
-        color = int(input("Enter Color Number to Replace (0-16): "))
+        row1 = int(st.number_input("Enter Row (0-2): "))
+        col1 = int(st.number_input("Enter Index to Replace Color (0-2): "))
+        color = int(st.number_input("Enter Color Number to Replace (0-16): "))
         
         two_d_arr[row1][col1] = color
-        print(two_d_arr)
         
         plt.imshow(two_d_arr, interpolation='none', cmap='Set3')
         plt.show()
+        st.pyplot(fig)
         
-        again = int(input("\n1 to Change, 2 to Finish: "))
+        again = int(st.number_input("\n1 to Change, 2 to Finish: "))
+        
         if again == 2:
             plt.imshow(two_d_arr, interpolation='none', cmap='Set3')
             plt.show()
