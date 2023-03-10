19d2d59cd2ada633c519721b6e2646e0c3fb2d50
hydraadra112 <126627743+hydraadra112@users.noreply.github.com>
2023-03-10 08:09:12 +0800

    Update Activity1_DDA_Line_Midpoint.py

--- pages/Activity1_DDA_Line_Midpoint.py
+++ pages/Activity1_DDA_Line_Midpoint.py
@@ -2,6 +2,7 @@ import streamlit as st
 import matplotlib.pyplot as plt
 st.title("Activity2\nGroup6\nDDALine_Midpoint")
 def DDALine (x1, y1, x2, y2):
+    fig = plt.figure
     dx = abs(x1 - x2)
     dy = abs(y1 - y2)
     
@@ -9,7 +10,7 @@ def DDALine (x1, y1, x2, y2):
     
     x3 = (x1+x2) / 2 #Midpoint of X
     y3 = (y1 + y2) / 2 # Midpoint of Y
-    print("Midpoint Line:", x3,"&", y3)
+    st.write("Midpoint Line:", x3,"&", y3)
     
     Xinc = float (dx / steps)
     Yinc = float (dy / steps)
@@ -26,18 +27,17 @@ def DDALine (x1, y1, x2, y2):
         x += Xinc
         y += Yinc
    
-    st.pyplot(xcords,ycords,marker="o", markersize=2, markerfacecolor="red")
-    st.pylot(x3,y3,marker="o", markersize=5, markerfacecolor="red")
-    st.line_chart()
+    plt.plot(xcords,ycords,marker="o", markersize=2, markerfacecolor="red")
+    plt.plot(x3,y3,marker="o", markersize=5, markerfacecolor="red")
+    plt.show()
+    st.pyplot(fig)
     
 def main () :
     
-    x1 = st.number_input('X1: ')
-    y1 = st.number_input('Y1: ')
-    x2 = st.number_input('X2: ')
-    y2 = st.number_input('Y2: ')
-    
-    
+    x1 = st.slider('X1: ', 0, 100, 1)
+    y1 = st.slider('Y1: ', 0, 100, 1)
+    x2 = st.slider('X2: ', 0, 100, 1)
+    y2 = st.slider('Y2: ', 0, 100, 1)
     DDALine (x1, y1, x2, y2)
     
 if __name__ == '__main__':
