42505cfc7f1e26e7cb6f28289bd6ffdf2b583e38
hydraadra112 <126627743+hydraadra112@users.noreply.github.com>
2023-03-16 18:50:21 +0800

    Update 0_Introduction.py

--- 0_Introduction.py
+++ 0_Introduction.py
@@ -4,3 +4,8 @@ st.set_page_config(
     page_title="Multipage - Group 6"
     
 )
+
+st.title("Group 6 - Neunzehn (BSCS 1-A)")
+st.write("Carado, John Manuel")
+st.write("Molet, Jonessa Marie")
+st.write("Pendon, Jeffrille Amber")
