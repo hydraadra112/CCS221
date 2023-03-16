429d8c4ebedcd309d4156741f4704817a8e0863b
hydraadra112 <126627743+hydraadra112@users.noreply.github.com>
2023-03-16 18:47:01 +0800

    Update 0_Introduction.py

--- 0_Introduction.py
+++ 0_Introduction.py
@@ -1,13 +1,7 @@
 import streamlit as st
 
-st.set_page_config(
+st.set_page_config
+(
     page_title="Multipage - Group 6"
     
 )
-
-st.title("Group 6 - CCS221")
-st.write('Streamlit Apps of Group 6 Neunzehn - CCS 221')
-
-st.write('\nThis app consists of 6 pages.\n')
-st.write('\n1ST Page\n')
-st.write('\tBresenhams Line\n\tDDA Line\n\tMidpoint Line')
