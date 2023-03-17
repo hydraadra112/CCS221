d510ebce184d39f94d615e33203c3ce693e58a09
hydraadra112 <126627743+hydraadra112@users.noreply.github.com>
2023-03-17 15:11:31 +0800

    Update 4_Activity_4_3D_Rotation.py

--- pages/4_Activity_4_3D_Rotation.py
+++ pages/4_Activity_4_3D_Rotation.py
@@ -29,7 +29,7 @@ def _plt_basic_object_(points):
     
     el = st.slider('Elevation: ', 0, 360, 1)
     az = st.slider('Azimuth: ', 0, 360, 1)
-    st.write('Recommended Position: AZI: 40, ELEV: 40')
+    st.write('Recommended Position: AZI: 45, ELEV: 20')
     ax.view_init(elev=el, azim=az)
     
     
